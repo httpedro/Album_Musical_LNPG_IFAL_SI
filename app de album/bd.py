c10a811a27b681966155f6de4848fb5ebe4b128f
Heverton Lourenço <30846708+HeevOL@users.noreply.github.com>
2022-04-12 01:26:25 -0300

    Arquivos referentes a versão 3.0
    O código da Versão 2.0 foi separado seguindo as regras de "aplicação de 3 camadas".
    
    Foi adicionado um arquivo txt que serve como banco de dados
    
    O código foi também comentado para melhor manutenção e entendimento de terceiros.

---
+++ app de album/bd.py
@@ -0,0 +1,70 @@
+# import os
+from tkinter import messagebox
+ 
+
+#Função Recebe Parametros da lista e adiciona os dados no arquivo_cadastro.txt
+def gravar_dados(album, ano, artista, lancamento):
+    # c=os.path.dirname(_file_)
+    # album_arquivo=c+"\\arquivo_cadastro.txt"
+    arquivo=open('app de album/arquivo_cadastro.txt',"a")
+
+    if(album=="" or ano=="" or artista=="" or lancamento==""):
+        messagebox.showinfo(title="ERRO", message="Preencha Todos os Dados")
+    else:    
+        if lancamento == 1:
+            lancamento = 'Sim'
+        else:
+            lancamento = 'Não'     
+
+        arquivo.write(f'{album}|{ano}|{artista}|{lancamento}\n')
+     
+    arquivo.close()
+
+# Função que cria uma matriz com todos os valores do banco de dados e a retorna para o domain.py
+def preenche_bd():
+    arquivo = open('app de album/arquivo_cadastro.txt',"r")
+    lista_completa = []
+
+    for dados in arquivo:
+        lista = dados.split('|')
+        lista_completa.append(lista)
+
+    arquivo.close()
+    return lista_completa
+
+# Função percorre a lista do banco de dados e verifica se o artista existe, listando assim elementos especificos na treeview 
+def preenche_artista(artista):
+    arquivo = open('app de album/arquivo_cadastro.txt',"r")
+    lista_completa = []
+
+    for dados in arquivo:
+        lista = dados.split('|')
+        if str(artista).lower() in lista[2].lower():
+            lista_completa.append(lista)
+
+    arquivo.close()
+    return lista_completa
+
+# Função recebe 2 parâmetros, percorre a lista e verifica o(s) ano(s) de acordo com a opção escolhida na ComboBox.
+# Retornando elementos especificos para a Treeview da tela
+def preenche_ano(op, ano):
+    arquivo = open('app de album/arquivo_cadastro.txt',"r")
+    lista_completa = []
+
+    for dados in arquivo:
+        lista = dados.split('|')
+
+        if op == "Anterior a":
+            if int(ano) >= int(lista[1]):
+                lista_completa.append(lista)
+
+        elif op == "Posterior a":
+            if int(ano) <= int(lista[1]):
+                lista_completa.append(lista)
+
+        elif op == "Igual a":
+            if int(ano) == int(lista[1]):
+                lista_completa.append(lista)
+
+    arquivo.close()
+    return lista_completa
---
+++ app de album/gui.py
@@ -0,0 +1,168 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import Combobox
+# Importa as funções do domain.py
+from domain import *
+
+
+# Função cria uma treeview 
+def treeview_bd(screem):
+
+    tv = ttk.Treeview(screem,columns=('Álbum', 'Ano de Lançamento', 'Banda/Artista', 'Lançamento do Artista'), show='headings')
+    tv.column('Álbum', minwidth=0,width=150)
+    tv.column('Ano de Lançamento', minwidth=0,width=125)
+    tv.column('Banda/Artista', minwidth=0,width=125)
+    tv.column('Lançamento do Artista', minwidth=0,width=125)
+    tv.heading('Álbum', text='Álbum')
+    tv.heading('Ano de Lançamento', text='Ano de Lançamento')
+    tv.heading('Banda/Artista', text='Banda/Artista')
+    tv.heading('Lançamento do Artista', text='Lançamento')
+    tv.pack(padx=10,pady=10,fill='both', expand=True)
+
+    return tv
+
+
+# Função criada para fazer a view da tabela que mostra o que tem no bd
+def banco_dados():
+    
+    bd_screem = Tk()
+    bd_screem.title("Banco de dados")
+    bd_screem.geometry("600x600")
+    bd_screem.configure(background="#FFDBE9")
+
+    bd_texto = Label(bd_screem, text="Deseja fazer buscas no banco de dados?",background="#FFDBE9")
+    bd_texto.pack()
+
+    btn_artista = Button(bd_screem, text='Realizar busca por artista', width=40,command=busca_por_artista)
+    btn_artista.pack()  
+    btn_ano = Button(bd_screem, text="Realizar busca por ano", width=40, command=busca_por_ano)  
+    btn_ano.pack()
+    # Chama a função para criar a TreeView
+    tv = treeview_bd(bd_screem)
+    # Chama a função do domain.py para preencher a TreeView que por sua vez fará o pedido para o bd.py
+    lista = preencher_bd()
+    # Preenche a TreeView
+    for dados in lista:
+        tv.insert('','end',values=(dados))   
+    
+    bd_screem.mainloop()         
+
+# Função que cria a tela de busca por artista
+def busca_por_artista():
+    # Essa Função apaga os dados da TreeView caso tenha algum, chama a função para preenche-la pelo domain.py
+    # enviando o artista digitado como parâmetro. Depois de receber a matriz com os devidos valores insere-os na TreeView
+    def busca(artista):
+        # Apaga
+        tv.delete(*tv.get_children())
+        # Chama
+        lista = preencher_artista(artista)
+        # Preenche
+        for dados in lista:
+            tv.insert('','end', values=(dados))
+
+
+    busca_artista_screem = Tk()
+    busca_artista_screem.title("Busca por Artista")
+    busca_artista_screem.geometry("600x600")
+    busca_artista_screem.configure(background="#FFDBE9")
+    
+    busca_artista_label = Label(busca_artista_screem, text="Digite o Artista/Banda que deseja procurar", background="#FFDBE9")
+    busca_artista_label.pack(ipady=10)
+    busca_artista_label2 = Label(busca_artista_screem, background="#FFDBE9")
+    busca_artista_label2.pack(side=TOP)
+
+    bd_busca_artista = Entry(busca_artista_screem, bd=2)
+    bd_busca_artista.place(x=10, y=40, width=500)
+    # Lambda permite enviar parametros dentro do command
+    btn = Button(busca_artista_screem, text="Buscar",width=8, command=lambda:busca(bd_busca_artista.get()))
+    btn.place(x=520, y=37)
+
+    tv = treeview_bd(busca_artista_screem)
+
+    busca_artista_screem.mainloop()
+
+
+# Função que cria a tela de busca por ano
+def busca_por_ano():
+    # Essa Função apaga os dados da TreeView caso tenha algum, chama a função para preenche-la pelo domain.py
+    # enviando a opção e o ano digitado como parâmetro. Depois de receber a matriz com os devidos valores insere-os na TreeView
+    def busca(op, ano):
+        # Apaga
+        tv.delete(*tv.get_children())
+        # Chama
+        lista = preencher_ano(op, ano)
+        # Preenche
+        for dados in lista:
+            tv.insert('','end', values=(dados))
+
+
+    busca_ano_screem = Tk()
+    busca_ano_screem.title("Busca por Ano")
+    busca_ano_screem.geometry("600x600")
+    busca_ano_screem.configure(background="#FFDBE9")
+
+    busca_ano_label = Label(busca_ano_screem, text="Preencha os campos para realizar a busca", background="#FFDBE9")
+    busca_ano_label.pack(ipady=10)
+    busca_ano_label2 = Label(busca_ano_screem, background="#FFDBE9")
+    busca_ano_label2.pack(side=TOP)
+
+    opcoes = ("Posterior a", "Anterior a", "Igual a")
+    op_ano = Combobox(busca_ano_screem, values=opcoes)
+    op_ano.place(x=100, y=40)
+    # Lambda permite enviar parametros dentro do command
+    bd_busca_ano = Entry(busca_ano_screem, bd=2)
+    bd_busca_ano.place(x=260, y=40)
+    btn = Button(busca_ano_screem, text="Buscar",width=15, command=lambda:busca(op_ano.get(), bd_busca_ano.get()))
+    btn.place(x=400, y=37)
+
+    tv = treeview_bd(busca_ano_screem)
+    busca_ano_screem.mainloop()
+
+
+# Função da view principal que cadastra banda
+def main():
+    # Função que grava dados no arquivo a partir dos parâmetros pegos nas Entrys e radioValue
+    # a função também limpa os dados da label após salvar os dados 
+    def grava_e_limpa_dados(album, ano, banda, lancamento):
+        # Chama a função para gravar
+        gravar_dados(album, ano, banda, lancamento)
+        # Limpa os dados nas entry e reseta o valor do radio
+        entry_album.delete(0,END)
+        entry_ano_lancamento.delete(0,END)
+        entry_banda.delete(0,END)
+        radioValue.set(1)
+    
+    programa = Tk()
+    programa.title("CADASTRO")
+    programa.geometry("500x500")
+    programa.configure(background="#FFDBE9")
+    
+    Label(programa, text="Escreva o nome do álbum",background="#FFDBE9",foreground="#000").pack()
+    entry_album=Entry(programa)
+    entry_album.pack()
+    
+    Label(programa, text="Escreva o ano de lancamento",background="#FFDBE9",foreground="#000").pack()
+    entry_ano_lancamento=Entry(programa)
+    entry_ano_lancamento.pack()
+    
+    Label(programa, text="Escreva o nome da banda/Artista",background="#FFDBE9",foreground="#000").pack()
+    entry_banda=Entry(programa)
+    entry_banda.pack()
+
+    radioValue=IntVar()
+    radioValue.set(1)
+    Label(programa, text="Primeiro Lançamento?",background="#FFDBE9",foreground="#000").pack()
+    radio1 = Radiobutton(programa, text='Sim',variable=radioValue, value=1, background="#FFDBE9",foreground="#000") 
+    radio2 = Radiobutton(programa, text='Não',variable=radioValue, value=2, background="#FFDBE9",foreground="#000")
+    radio1.pack()
+    radio2.pack()
+
+    # Lambda permite enviar parâmetros dentro do command
+    Button(text="Salvar",width=25, command=lambda:grava_e_limpa_dados(entry_album.get(), entry_ano_lancamento.get(), entry_banda.get(), radioValue.get())).pack(pady="5")
+    Button(text="Banco de dados",width=25, command=banco_dados).pack()
+    
+    programa.mainloop()
+
+
+# Executa o programa
+main()
